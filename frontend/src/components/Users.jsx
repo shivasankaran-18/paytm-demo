1865e1426d2e9b95eb76dbc7706df1de3c38dfc7
shivasankaran-18 <148421597+shivasankaran-18@users.noreply.github.com>
2024-05-10 15:45:24 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -7,6 +7,15 @@ import { useNavigate } from "react-router-dom";
 export const Users = () => {
     const [users, setUsers] = useState([]);
     const [filter, setFilter] = useState(" ");
+    let timeout;
+    function fn(val)
+    {
+        clearTimeout(timeout);
+        timeout=setTimeout(()=>
+        {
+            setFilter(val)
+        },1000)
+    }
 
     useEffect(() => {
         
@@ -28,7 +37,7 @@ export const Users = () => {
             Users
         </div>
         <div className="my-2">
-            <input onChange={(e)=>setFilter(e.target.value)}
+            <input onChange={(e)=>fn(e.target.value)}
              type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
 
